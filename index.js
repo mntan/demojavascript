a5c14bda5c779523dff13ec104b4b5cac8cb826a
TanNguyen <mntan998@gmail.com>
2016-10-15 19:19:46 +0700

    [js] Add index file with console log

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by thangnv on 5/18/15.
+ */
+    "use strict";
+var str1='This is demo javascript on webstorm';
+console.log(str1);//This is demo javascript on webstorm
+console.log(typeof str1);//string
+var number1=10;
+console.log(number1);
+console.log(typeof number1);//number
+number1 = 11 ;
+console.log('Change number value to : '+number1);
+let y = 20;
+console.log(y);
+console.log(typeof y);//number
+const z = 30 ;
+console.log('Value of const var : ' + z);
+console.log(typeof z);//number
+//z= 31;
+
+//output
+/*
+ This is demo javascript on webstorm
+ string
+ 10
+ number
+ Change number value to : 11
+ 20
+ number
+ Value of const var : 30
+ number
+ */
